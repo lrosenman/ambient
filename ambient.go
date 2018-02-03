d9870b937a0ba2b0b6b150a7a00e51a6eb90957e
Larry Rosenman <larryrtx@gmail.com>
2018-02-03 15:29:47 -0600

    clarify the Uv BUG() entry

--- ambient.go
+++ ambient.go
@@ -77,8 +77,8 @@ type Record struct {
 	// BUG(lrosenman): Totalrainin should be float64
 	Totalrainin string
 	// BUG(lrosenman): Uv should be an int
-	// but the test device is reporting a float
-	// but per https://www.epa.gov/sunsafety/calculating-uv-index-0
+	// but the WS-8478 device is reporting a float.
+	// Per https://www.epa.gov/sunsafety/calculating-uv-index-0
 	// it should be an int.
 	// all consoles EXCEPT the WS-8478 return int, but we have to 
 	// accommodate the WS-8478.
